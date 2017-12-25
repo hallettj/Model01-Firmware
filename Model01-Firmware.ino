937cd48fb901601b27fe9e2cffcfc3c23ace93cb
Jesse Hallett <jesse@sitr.us>
2017-12-25 19:25:19 -0400

    Activate stalker LED effect by default

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -366,7 +366,8 @@ void setup() {
   // We want to make sure that the firmware starts with LED effects off
   // This avoids over-taxing devices that don't have a lot of power to share
   // with USB devices
-  LEDOff.activate();
+  //LEDOff.activate();
+  StalkerEffect.activate();
 }
 
 /** loop is the second of the standard Arduino sketch functions.
